626e8c7226ca96b4d18a9b1baebf3343d2fbc705
Amarjeet <amarjeet.billfree@gmail.com>
2022-06-08 14:42:18 +0530

    sum zero

---
+++ arrays/sumZero.js
@@ -0,0 +1,33 @@
+//Given a sorted array of integers find all the pair where sum of 2 elements is 0
+//Input : [-3,-2,-1, 0, 2, 3, 5, 6]
+//Output : [[-3, 3], [-2, 2]]
+
+//Time - O(n) where n - length of given array
+
+function sumZero(arr) {
+	let left = 0
+	let right = arr.length - 1
+	let result = []
+
+	//avoid the iteration if the first element it self is a +ve value
+	if (arr[0] >= 0) {
+		return result
+	}
+
+	while (left < right) {
+		let sum = arr[left] + arr[right]
+		if (sum == 0) {
+			result.push([arr[left], arr[right]])
+		}
+		if (sum > 0) {
+			right--
+		} else {
+			left++
+		}
+	}
+
+	return result
+}
+
+//sample test
+console.log(sumZero([-3, -1, 0, 1, 2, 4, 3]));
