39801c07b383d84aaaae4c3b72b20d51a8d0b90f
Amarjeet <amarjeet.billfree@gmail.com>
2022-06-08 15:04:21 +0530

    count unique

---
+++ arrays/countUnique.js
@@ -0,0 +1,25 @@
+//given a sorted array count the unique values in the array; consider -ve values as different value of its positive one
+//Input : [-3,-2,-2,-1, 0, 2]
+//Output : 5
+
+//Time - O(n) where n is length of array
+
+function countUnique(arr) {
+	let count = 0
+	let i = 0
+	let j = i + 1
+
+	while (j <= arr.length) {
+		if (arr[i] != arr[j]) {
+			//move i to different number index
+			i = j
+			count++
+		}
+		j++
+	}
+
+	return count
+}
+
+// Sample Test
+console.log(countUnique([1,1,1,1,2]));
